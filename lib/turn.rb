542238bcab0c3dbbc51916c2a7d7b750a7c16752
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 01:27:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,3 +2,12 @@ def display_board(board)
     puts " board[0] | board[1] | board[2]"
     puts " board[3] | board[4] | board[5]"
     puts " board[6] | board[7] | board[8]"
+end
+
+def position_taken?(board, index)
+    if (board[index] == "" || board[index] == " " || board[index] == nil)
+        false
+    elsif (board[index] == "O" || board[index] == "X")
+        turn
+    end
+end
